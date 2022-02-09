0ec9a54d71b848806471e9617698240d3215c833
GiftBanda <bandagift42@gmail.com>
2022-02-09 11:30:25 +0200

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Hero = () => {
+    return (
+        <div className="hero">
+        <div className="hero-text">
+            <h1>Welcome to the</h1>
+            <h1>Worlds best freelancer</h1>
+            <h2>Gift Banda</h2>
+        </div>
+        </div>
+    )
+}
+
+export default Hero;
